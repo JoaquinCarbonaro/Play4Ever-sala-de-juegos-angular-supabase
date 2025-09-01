77c228c7af602e686157fdf94ee83b8d87bea538
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-09-01 16:36:18 -0300

    add: creacion del proyecto

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Joaquin-Carbonaro-TP1-PROG4-2025-C2');
+}
