8ddd03281639356b38ddbb084fc34d23e6341776
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-09-03 21:08:33 -0300

    add: comentarios componentes

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,12 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common"
+import { Component } from "@angular/core"
 
 @Component({
-  selector: 'app-footer',
+  selector: "app-footer",
   imports: [CommonModule],
-  templateUrl: './footer.html',
-  styleUrl: './footer.css'
+  templateUrl: "./footer.html",
+  styleUrl: "./footer.css",
 })
 export class Footer {
+  //guardo el año actual
   currentYear = new Date().getFullYear()
 }
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Component } from "@angular/core"
+import { RouterModule } from "@angular/router"
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap"
 
 @Component({
-  selector: 'app-navbar',
+  selector: "app-navbar",
   imports: [RouterModule, NgbModule],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  templateUrl: "./navbar.html",
+  styleUrl: "./navbar.css",
 })
 export class Navbar {
+  //guarda estado del menu (cerrado o abierto)
   isMenuCollapsed = true
 
+  //abre o cierra el menu cuando toco el boton
   toggleMenu() {
     this.isMenuCollapsed = !this.isMenuCollapsed
+  }
+
+  //metodo para cerrar el menu al cambiar de pagina
+  closeMenu() {
+    this.isMenuCollapsed = true;
   }
 }
