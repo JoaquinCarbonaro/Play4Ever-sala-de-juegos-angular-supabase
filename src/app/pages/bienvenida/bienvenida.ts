99e69a051c96201f9f1bc7b3548a8b8aa78cdf89
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-09-11 15:10:54 -0300

    add: logica sesion bienvenida

--- src/app/pages/bienvenida/bienvenida.ts
+++ src/app/pages/bienvenida/bienvenida.ts
@@ -1,43 +1,58 @@
-import { CommonModule } from "@angular/common"
-import { Component } from "@angular/core"
-import { RouterModule } from "@angular/router"
+import { AsyncPipe  } from '@angular/common'
+import { Component, inject } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { Auth } from '../../services/auth'
+import { Observable } from 'rxjs'
+import { Usuario } from '../../classes/usuario'
 
 @Component({
-  selector: "app-bienvenida",
-  imports: [RouterModule, CommonModule],
-  templateUrl: "./bienvenida.html",
-  styleUrl: "./bienvenida.css",
+  selector: 'app-bienvenida',
+  imports: [RouterModule, AsyncPipe], //AsyncPipe para no tener que suscribirme manualmente al observable
+  templateUrl: './bienvenida.html',
+  styleUrls: ['./bienvenida.css'],
 })
 export class Bienvenida {
   //lista de juegos
   juegos = [
     {
-      nombre: "Ahorcado",
-      descripcion: "Adivina la palabra oculta antes de que se complete el dibujo",
-      icono: "bi-alphabet",
+      nombre: 'Ahorcado',
+      descripcion: 'Adivina la palabra oculta antes de que se complete el dibujo',
+      icono: 'bi-alphabet',
       disponible: false,
       sprint: 3,
     },
     {
-      nombre: "Mayor o Menor",
-      descripcion: "Juego de cartas donde debes adivinar si la siguiente carta es mayor o menor",
-      icono: "bi-suit-spade",
+      nombre: 'Mayor o Menor',
+      descripcion: 'Juego de cartas donde debes adivinar si la siguiente carta es mayor o menor',
+      icono: 'bi-suit-spade',
       disponible: false,
       sprint: 3,
     },
     {
-      nombre: "Preguntados",
-      descripcion: "Trivia con preguntas de diferentes categorías",
-      icono: "bi-question-circle",
+      nombre: 'Preguntados',
+      descripcion: 'Trivia con preguntas de diferentes categorias',
+      icono: 'bi-question-circle',
       disponible: false,
       sprint: 4,
     },
     {
-      nombre: "Connect 4",
-      descripcion: "Conecta cuatro fichas en línea para ganar",
-      icono: "bi-grid-3x3",
+      nombre: 'Connect 4',
+      descripcion: 'Conecta cuatro fichas en linea para ganar',
+      icono: 'bi-grid-3x3',
       disponible: false,
       sprint: 4,
     },
   ]
+
+  //observable que guarda el estado del usuario
+  usuario$: Observable<Usuario | null>
+
+  //inyecto servicio de auth
+  private readonly auth = inject(Auth)
+
+  constructor() {
+    //asigno observable de usuario desde auth
+    this.usuario$ = this.auth.currentUser$
+  }
+  
 }
