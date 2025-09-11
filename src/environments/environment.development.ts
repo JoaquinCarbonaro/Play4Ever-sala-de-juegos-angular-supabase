22c70d600f0572c8f7a21d0a869f54d1f33483a3
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-09-11 12:50:42 -0300

    add: conexion con supabase e instalacion sweetAlert2

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,11 +3,11 @@ export const environment = {
   //indica si estamos en produccion o no
   production: false,
   //url de supabase para la base de datos
-  supabaseUrl: "https://xxxxxxx.supabase.co",
+  supabaseUrl: 'https://hzaovdaagjazokgunvfe.supabase.co',
   //clave publica de supabase
-  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...", // anon key publica
-  //url de la api de github
-  githubApiUrl: "https://api.github.com",
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6YW92ZGFhZ2phem9rZ3VudmZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyMDExNDMsImV4cCI6MjA3Mjc3NzE0M30._YVxdstpN3aRuCT1cQB6XqHw4B_mQ9o2WjaMeWVPKO0', // anon key publica
+  githubApiUrl: 'https://api.github.com',
   //nombre de usuario de github para mostrar en sobre mi
-  githubUsername: "JoaquinCarbonaro",
-}
+  githubUsername: 'JoaquinCarbonaro',
+};
