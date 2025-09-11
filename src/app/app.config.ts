97d3e09b641e0f142667ab8e7c5be7fdf1c97da3
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-09-11 15:00:15 -0300

    delete: .spec.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { type ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from "@angular/core"
 import { provideRouter } from "@angular/router"
 import { provideHttpClient } from "@angular/common/http"
-//importo las rutas de la app
 import { routes } from "./app.routes"
 
 //configuracion principal de la aplicacion
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    //zoneless
     provideZonelessChangeDetection(),
     //provee el router con las rutas definidas
     provideRouter(routes),
