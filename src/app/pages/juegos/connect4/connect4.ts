fae94ab5114b66209e0c7869c4d01e33e066ad3a
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-09-17 14:14:59 -0300

    add: estructura del proyecto

---
+++ src/app/pages/juegos/connect4/connect4.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connect4',
+  imports: [],
+  templateUrl: './connect4.html',
+  styleUrl: './connect4.css'
+})
+export class Connect4 {
+
+}
