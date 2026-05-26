1827ff07f320361a70a5e4868a5a7bb56b67a45c
Joaquin Carbonaro <138243106+JoaquinCarbonaro@users.noreply.github.com>
2026-05-26 17:16:23 -0300

    Replace Supabase anon key with placeholder

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -6,7 +6,7 @@ export const environment = {
   supabaseUrl: 'https://hzaovdaagjazokgunvfe.supabase.co',
   //clave publica de supabase
   supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6YW92ZGFhZ2phem9rZ3VudmZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyMDExNDMsImV4cCI6MjA3Mjc3NzE0M30._YVxdstpN3aRuCT1cQB6XqHw4B_mQ9o2WjaMeWVPKO0', // anon key publica
+    'SUPABASE_ANON_KEY_PUBLICA', // anon key publica
   githubApiUrl: 'https://api.github.com',
   //nombre de usuario de github para mostrar en sobre mi
   githubUsername: 'JoaquinCarbonaro',
